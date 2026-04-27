2154dac1b4f1eeea379bb476934604b43ac37dae
Kent Anthony Engbino <kentanthony070495@gmail.com>
2026-04-27 23:27:28 +0800

    Updated

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -214,7 +214,7 @@
                                             {{ shop.type }}
                                         </span>
                                     </div>
-                                    <div class="d-flex align-center flex-column"
+                                    <div :class="{ 'd-none' : !shop.lowest_price }" class="d-flex align-center flex-column"
                                         style="position: absolute; right: 10px;">
                                         <span class="text-grey-darken-1" style="font-size:10px">
                                             Starting @
